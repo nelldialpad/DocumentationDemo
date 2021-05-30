6c04323933e3ff1639e25960ef2baffa8fdcdffa
nelldialpad <77693527+nelldialpad@users.noreply.github.com>
2021-05-30 13:22:24 -0700

    Create myQueries.py

---
+++ myQueries.py
@@ -0,0 +1,3 @@
+abandon_rate_query = "SELECT * FROM nell_is_awesome WHERE __key__.id = 'abandon_rate_pretend_query'
+
+abandoned_calls_query = "SELECT * FROM nell_is_awesome WHERE __key__.id = 'abandoned_calls_pretend_query'
